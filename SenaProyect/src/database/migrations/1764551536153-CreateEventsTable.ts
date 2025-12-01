66b6206e42388016f944a38164158d42ef926654
AngelDaviid <angel_dvalenciae@soy.sena.edu.co>
2025-11-30 20:40:09 -0500

    migraciones

---
+++ SenaProyect/src/database/migrations/1764551536153-CreateEventsTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateEventsTable1764551536153 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ SenaProyect/src/database/migrations/1764551788485-AddLocationToEvents.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddLocationToEvents1764551788485 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
